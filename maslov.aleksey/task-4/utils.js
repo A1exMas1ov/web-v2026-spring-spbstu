e92b1bcf299c78ab1fbd751f2f3bac8783790469
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2026-04-06 17:01:37 +0300

    add funcs: group..., unique supplier, etc (4.2)

---
+++ maslov.aleksey/task-4/utils.js
@@ -0,0 +1,39 @@
+function groupBySupplier(products) {
+    const map = {};
+    for (const p of products) {
+        for (const s of p.suppliers) {
+            if (!map[s]) {
+                map[s] = [];
+            }
+            map[s].push(p);
+        }
+    }
+    return map;
+}
+
+function uniqueSuppliers(products) {
+    return [...new Set(products.flatMap(p => p.suppliers))].sort();
+}
+
+function productsBySupplier(products, supplierName) {
+    return products.filter(p => p.suppliers.includes(supplierName));
+}
+
+function groupBySupplierCount(products) {
+    const map = {};
+    for (const p of products) {
+        if (!map[p.supplierCount]) {
+            map[p.supplierCount] = [];
+        }
+        map[p.supplierCount].push(p);
+    }
+    return map;
+}
+
+function maxSupplierProducts(products) {
+  if (!products.length) {
+    return [];
+  }
+  const max = Math.max(...products.map(p => p.supplierCount));
+  return products.filter(p => p.supplierCount === max);
+}
